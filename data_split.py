c229b2198cd4c3477fa871261c8eb6c13b912a9e
Jihoon Lee <leejh98123@knu.ac.kr>
2022-10-14 19:07:35 +0900

    first

---
+++ data_split.py
@@ -0,0 +1,51 @@
+#%%
+import pandas as pd
+import json
+
+with open('C:/Users/PC/Desktop/4학년 2학기/졸업프로젝트/coco_data/annotation/MSCOCO_train_val_Korean.json') as f:
+    json_data = json.load(f)
+
+json_data=pd.DataFrame(json_data)
+json_data['file_path']=json_data['file_path'].apply(lambda x : 'C:/Users/PC/Desktop/4학년 2학기/졸업프로젝트/coco_data/annotation/'+x)
+
+def remove_text_len_100(input_list_text):
+
+    list_result = [text for text in input_list_text if len(text)<=100 and text!='']
+
+    return list_result
+
+json_data['caption_ko'] = json_data['caption_ko'].apply(lambda x : remove_text_len_100(x))
+
+json_train_data=json_data[json_data['file_path'].str.contains('train')] 
+json_train_data['caption_ko']=json_train_data['caption_ko'].apply(lambda x : '!@#'.join(x))
+
+json_train_data = json_train_data.head(100)
+
+train_data=json_train_data['caption_ko'].str.split('!@#') 
+train_data=train_data.apply(lambda x : pd.Series(x)) 
+train_data = train_data.stack().reset_index(level=1, drop=True).to_frame('caption_ko') 
+json_train_data.drop('caption_ko',axis=1,inplace=True)
+
+train_data = json_train_data.merge(train_data, left_index=True, right_index=True, how='left') 
+train_data.reset_index(drop=True,inplace=True)
+# train_data['caption_ko']=train_data['caption_ko'].apply(lambda x : re.sub('(?:\.)','',x).strip())
+
+print(train_data)
+
+#%%
+json_valid_data = json_data[json_data['file_path'].str.contains('val')]
+# json_valid_data = json_valid_data.head(8)
+json_valid_data['caption_ko'] = json_valid_data['caption_ko'].apply(lambda x: x[0])
+
+valid_data= json_valid_data['caption_ko'].str.split('!@#') 
+valid_data= valid_data.apply(lambda x : pd.Series(x)) 
+valid_data = valid_data.stack().reset_index(level=1, drop=True).to_frame('caption_ko') 
+json_valid_data.drop('caption_ko',axis=1,inplace=True)
+
+valid_data = json_valid_data.merge(valid_data, left_index=True, right_index=True, how='left') 
+valid_data.reset_index(drop=True,inplace=True)
+
+print(valid_data)
+
+
+# %%
